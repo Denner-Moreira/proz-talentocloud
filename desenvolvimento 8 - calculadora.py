fa3be39f8abb5eceee7d66e23175f14ec8975bb1
Denner-Moreira <142538210+Denner-Moreira@users.noreply.github.com>
2023-08-19 16:45:20 -0300

    Update desenvolvimento 8 - calculadora.py

--- desenvolvimento 8 - calculadora.py
+++ desenvolvimento 8 - calculadora.py
@@ -1,11 +1,11 @@
 def calculadora(numero1, numero2, operacao):
-    if operacao == 1:  # Soma
+    if operacao == 1:  
         resultado = numero1 + numero2
-    elif operacao == 2:  # Subtração
+    elif operacao == 2:  
         resultado = numero1 - numero2
-    elif operacao == 3:  # Multiplicação
+    elif operacao == 3:  
         resultado = numero1 * numero2
-    elif operacao == 4:  # Divisão
+    elif operacao == 4:  
         if numero2 != 0:
             resultado = numero1 / numero2
         else:
@@ -16,3 +16,11 @@ def calculadora(numero1, numero2, operacao):
         return 0
     
     return resultado
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+op = int(input("Digite o número da operação (1 para Soma, 2 para Subtração, 3 para Multiplicação, 4 para Divisão): "))
+
+resultado = calculadora(num1, num2, op)
+if resultado is not None:
+    print("Resultado:", resultado)
